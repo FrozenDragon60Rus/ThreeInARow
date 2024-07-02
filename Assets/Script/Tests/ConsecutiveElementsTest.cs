e396f129cdefe49ecb5c24d519209ec4121c9676
FrozenDragon60Rus <naum6666@yandex.ru>
2024-07-02 10:34:59 +0300

    rewrite generate

--- Assets/Script/Tests/ConsecutiveElementsTest.cs
+++ Assets/Script/Tests/ConsecutiveElementsTest.cs
@@ -19,12 +19,12 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator ConsecutiveElementsSearchFromStartTest()
+        public IEnumerator SearchFromStartTest()
         {
             List<Cell> cell = new(),
                        delete = new();
-            Generate.Cell(cell);
-            Generate.Element(cell);
+            cell.Generate();
+            cell.Element();
 
             var search = new ConsecutiveElements(10, 10, 0);
             delete.AddRange(
@@ -40,12 +40,36 @@ namespace Tests
             yield return null;
         }
 		[UnityTest]
-		public IEnumerator ConsecutiveElementsSearchFromElementTest()
+		public IEnumerator SearchFromStartStaticTest()
 		{
 			List<Cell> cell = new(),
 					   delete = new();
-			Generate.Cell(cell, 1, 10);
-			Generate.StaticElement(cell);
+			cell.Generate(3, 10);
+			cell.StaticElement30();
+
+			var search = new ConsecutiveElements(3, 10, 0);
+			delete.AddRange(
+				search.FindFromStart(cell, new ElementType[] { ElementType.Red,
+															   ElementType.Green,
+															   ElementType.Blue,
+															   ElementType.Yellow
+															 }
+									)
+						   );
+
+			foreach (var element in delete)
+				Debug.Log(element.row * 10 + element.col);
+
+			Assert.AreEqual(16, delete.Count);
+			yield return null;
+		}
+		[UnityTest]
+		public IEnumerator SearchFromElementTest()
+		{
+			List<Cell> cell = new(),
+					   delete = new();
+			cell.Generate(1, 10);
+			cell.StaticElement10();
 
 			var search = new ConsecutiveElements(1, 10, 0);
 			delete.AddRange(
@@ -55,18 +79,45 @@ namespace Tests
 			yield return null;
 		}
 		[UnityTest]
-		public IEnumerator ConsecutiveElementsSearchFromElementZeroTest()
+		public IEnumerator SearchFromElementZeroTest()
 		{
 			List<Cell> cell = new(),
 					   delete = new();
-			Generate.Cell(cell, 1, 10);
-			Generate.StaticElement(cell);
+			cell.Generate(1, 10);
+			cell.StaticElement10();
 
+			Debug.Log("start "+delete.Count);
 			var search = new ConsecutiveElements(1, 10, 0);
 			delete.AddRange(
 				search.FindFromElement(cell, cell[1], null));
+			Debug.Log("end " + delete.Count);
 
 			Assert.Zero(delete.Count);
+			yield return null;
+		}
+		[UnityTest]
+		public IEnumerator GetFromCrossTest()
+		{
+			List<Cell> cell = new(),
+					   delete = new();
+			cell.Generate(3, 10);
+			cell.StaticElement30();
+
+			var consecutive = new ConsecutiveElements(3, 10, 0);
+			var type = consecutive.GetType();
+			var flag = BindingFlags.Instance | BindingFlags.NonPublic;
+			var getDirection = type.GetMethod("GetFromCross", flag);
+			var parameters = new object[] 
+			{ 
+				cell, 
+				new Cell[]{ cell[0], cell[1], cell[2] },
+				delete,
+				1
+			};
+
+			int direction = (int)getDirection?.Invoke(consecutive, parameters);
+
+			Assert.AreEqual(delete, delete);
 			yield return null;
 		}
 
@@ -92,14 +143,20 @@ namespace Tests
         {
             List<Cell> cell = new();
 
-			Generate.Cell(cell);
-			Generate.Element(cell);
+			cell.Generate();
+			cell.Element();
 
 			Func<Cell, int, bool> row = (c, pos) => c.row == pos;
 
 			var consecutive = new ConsecutiveElements(10, 10, 0);
 			var type = consecutive.GetType();
 			var flag = BindingFlags.Instance | BindingFlags.NonPublic;
+
+			var rangeEnum = type.GetEnumNames();
+
+			var rangeFlag = type.GetField("range", flag);
+			rangeFlag.SetValue(consecutive, 1);
+
 			var elementsCount = type.GetMethod("GetDirection", flag);
 			var parameters = new object[] 
             {
